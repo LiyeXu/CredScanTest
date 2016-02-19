a3b1511f442159d12651e3fbdc3ad5edca1554cb
Eric Xu <ericxu@microsoft.com>
2016-02-19 14:33:17 -0800

    test

--- CredScanGitHubTestRepo/CredScanGitHubTestRepo/Program.cs
+++ CredScanGitHubTestRepo/CredScanGitHubTestRepo/Program.cs
@@ -11,6 +11,7 @@ namespace CredScanGitHubTestRepo
         static void Main(string[] args)
         {
             var storageAccount = "DefaultEndpointsProtocol=https;AccountName=credscantest;AccountKey=zPHCADQfmasyzTOP3hdPc1rAZfLI4CatAgJaDcsC1yDveIkpv+Q4bPm/Ar17GI3Rh7f0YWZ/A8Ab5l+VlNw72q==;BlobEndpoint=https://credscantest.blob.core.windows.net/;TableEndpoint=https://credscantest.table.core.windows.net/;QueueEndpoint=https://credscantest.queue.core.windows.net/;FileEndpoint=https://credscantest.file.core.windows.net/";
+            var storageAccount2 = "DefaultEndpointsProtocol=https;AccountName=credscantest;AccountKey=zPHCADQfmasyzTOP3hdPc1rAZfLI4CatAgJaDcsC1yDveIkpv+Q4bPm/Ar17GI3Rh7f0YWZ/A8Ab5l+VlNw72q==;BlobEndpoint=https://credscantest.blob.core.windows.net/;TableEndpoint=https://credscantest.table.core.windows.net/;QueueEndpoint=https://credscantest.queue.core.windows.net/;FileEndpoint=https://credscantest.file.core.windows.net/";
         }
     }
 }
