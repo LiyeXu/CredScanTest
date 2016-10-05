f840bc7010c8f70cfd9f58d62c4a37c08b9e1344
Eric Xu <ericxu@microsoft.com>
2016-10-05 15:16:43 -0700

    add publish setting test file

--- CredScanGitHubTestRepo/CredScanGitHubTestRepo/Program.cs
+++ CredScanGitHubTestRepo/CredScanGitHubTestRepo/Program.cs
@@ -9,12 +9,7 @@ namespace CredScanGitHubTestRepo
     class Program
     {
         static void Main(string[] args)
-        {
-            var storageAccount = "DefaultEndpointsProtocol=https;AccountName=credscantest;AccountKey=zPHCADQfmasyzTOP3hdPc1rAZfLI4CatAgJaDcsC1yDveIkpv+Q4bPm/Ar17GI3Rh7f0YWZ/A8Ab5l+VlNw72q==;BlobEndpoint=https://credscantest.blob.core.windows.net/;TableEndpoint=https://credscantest.table.core.windows.net/;QueueEndpoint=https://credscantest.queue.core.windows.net/;FileEndpoint=https://credscantest.file.core.windows.net/";
-            var storageAccount2 = "DefaultEndpointsProtocol=https;AccountName=credscantest;AccountKey=zPHCADQfmasyzTOP3hdPc1rAZfLI4CatAgJaDcsC1yDveIkpv+Q4bPm/Ar17GI3Rh7f0YWZ/A8Ab5l+VlNw72q==;BlobEndpoint=https://credscantest.blob.core.windows.net/;TableEndpoint=https://credscantest.table.core.windows.net/;QueueEndpoint=https://credscantest.queue.core.windows.net/;FileEndpoint=https://credscantest.file.core.windows.net/";
-            var storageAccount3 = "DefaultEndpointsProtocol=https;AccountName=credscantest;AccountKey=zPHCADQfmasyzTOP3hdPc1rAZfLI4CatAgJaDcsC1yDveIkpv+Q4bPm/Ar17GI3Rh7f0YWZ/A8Ab5l+VlNw72q==;BlobEndpoint=https://credscantest.blob.core.windows.net/;TableEndpoint=https://credscantest.table.core.windows.net/;QueueEndpoint=https://credscantest.queue.core.windows.net/;FileEndpoint=https://credscantest.file.core.windows.net/";
-            var storageAccount4 = "DefaultEndpointsProtocol=https;AccountName=credscantest;AccountKey=zPHCADQfmasyzTOP3hdPc1rAZfLI4CatAgJaDcsC1yDveIkpv+Q4bPm/Ar17GI3Rh7f0YWZ/A8Ab5l+VlNw72q==;BlobEndpoint=https://credscantest.blob.core.windows.net/;TableEndpoint=https://credscantest.table.core.windows.net/;QueueEndpoint=https://credscantest.queue.core.windows.net/;FileEndpoint=https://credscantest.file.core.windows.net/";
-            var storageAccount5 = "DefaultEndpointsProtocol=https;AccountName=credscantest;AccountKey=zPHCADQfmasyzTOP3hdPc1rAZfLI4CatAgJaDcsC1yDveIkpv+Q4bPm/Ar17GI3Rh7f0YWZ/A8Ab5l+VlNw72q==;BlobEndpoint=https://credscantest.blob.core.windows.net/;TableEndpoint=https://credscantest.table.core.windows.net/;QueueEndpoint=https://credscantest.queue.core.windows.net/;FileEndpoint=https://credscantest.file.core.windows.net/";
+        {            
         }
     }
 }
