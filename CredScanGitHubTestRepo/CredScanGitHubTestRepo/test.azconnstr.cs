15a7add2faf0f13384581f6d6b7c72037744eb22
Eric Xu <ericxu@microsoft.com>
2017-04-04 13:10:52 -0700

    Add az sql conn str

---
+++ CredScanGitHubTestRepo/CredScanGitHubTestRepo/test.azconnstr.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CredScanGitHubTestRepo
+{
+    class test
+    {
+        string connstr = "Data Source=tcp:aaa.database.windows.net,1433;Initial Catalog=bbb;Integrated Security=False;User ID=ccc@ww4wobk87p;Password=ddd;";
+    }
+}
