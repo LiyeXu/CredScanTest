59e14a471f7868d7f33653138f2edb8f3268f3b3
Eric Xu <ericxu@microsoft.com>
2017-06-27 15:20:20 -0700

    Test storage account detection

---
+++ CredScanGitHubTestRepo/CredScanGitHubTestRepo/storageAccount.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CredScanGitHubTestRepo
+{
+    class storageAccount
+    {
+        static string ConnStr = "AccountName=test;AccountKey=abcdefghijklmnopqrstuvwxyz0123456789/+ABCDEabcdefghijklmnopqrstuvwxyz0123456789/+ABCDE==";
+    }
+}
