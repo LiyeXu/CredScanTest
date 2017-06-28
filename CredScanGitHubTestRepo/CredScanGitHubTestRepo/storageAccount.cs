d9f69e0356ffd90bd1caabb70f336027f8c5d76d
Eric Xu <ericxu@microsoft.com>
2017-06-27 18:09:08 -0700

    update

--- CredScanGitHubTestRepo/CredScanGitHubTestRepo/storageAccount.cs
+++ CredScanGitHubTestRepo/CredScanGitHubTestRepo/storageAccount.cs
@@ -8,8 +8,6 @@ namespace CredScanGitHubTestRepo
 {
     class storageAccount
     {
-
-        static string ConnStr = "AccountName=test;AccountKey=abcdefghijklmnopqrstuvwxyz0123456789/+ABCDEabcdefghijklmnopqrstuvwxyz0123456789/+ABCDE==";
-
+            static string ConnStr = "AccountName=test;AccountKey=abcdefghijklmnopqrstuvwxyz0123456789/+ABCDEabcdefghijklmnopqrstuvwxyz0123456789/+ABCDE==";
     }
 }
