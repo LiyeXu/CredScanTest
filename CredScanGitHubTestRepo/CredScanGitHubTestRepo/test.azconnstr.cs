634157915ea3a3e6e98b8f69cdb11aa5f3457ece
Eric Xu <ericxu@microsoft.com>
2017-08-29 11:15:55 -0700

    test az conn str

--- CredScanGitHubTestRepo/CredScanGitHubTestRepo/test.azconnstr.cs
+++ CredScanGitHubTestRepo/CredScanGitHubTestRepo/test.azconnstr.cs
@@ -12,6 +12,6 @@ namespace CredScanGitHubTestRepo
            // This is a mockup sql azure connection string to be detected.
            string connstr = "Data Source=tcp:aaa.database.windows.net,1433;Initial Catalog=bbb;Integrated Security=False;User ID=ccc@aaa;Password=ddd;";
            string connstr2 = "server=tcp:eee.database.windows.net,1433;database=fff;Integrated Security=False;user=ggg@aaa;pwd=hhh;";
-              string connstr3 = "server=tcp:credscandev.database.windows.net,1433;database=credscandev;Integrated Security=False;user=redactedusername;pwd=redactedpwd;";
+           string connstr3 = "server=tcp:credscandev.database.windows.net,1433;database=credscandev;Integrated Security=False;user=credscansmoketestuser;pwd=credscansmoketestpwd;";
     }
 }
