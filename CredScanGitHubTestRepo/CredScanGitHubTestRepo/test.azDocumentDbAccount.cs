e8dd13db1a73dd34a472f4564c9d89649d20e2cd
Eric Xu <ericxu@microsoft.com>
2017-10-16 11:29:56 -0700

    Add document db connection string

---
+++ CredScanGitHubTestRepo/CredScanGitHubTestRepo/test.azDocumentDbAccount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CredScanGitHubTestRepo
+{
+
+    class TestAzureDocumentDbAccount
+    {
+           // This is a mockup sql azure connection string to be detected.
+           string connstr = "AccountEndpoint=https://test.documents.azure.com:443/;AccountKey=abcdefghijklmnopqrstuvwxyz0123456789/+ABCDEabcdefghijklmnopqrstuvwxyz0123456789/+ABCDE==;";
+    }
+}
