832258eeb1d23d00d342498b836aa0eb558179e4
Eric Xu <ericxu@microsoft.com>
2017-10-30 12:54:40 -0700

    add new storage account key

--- CredScanGitHubTestRepo/CredScanGitHubTestRepo/test.azStorageAccount.cs
+++ CredScanGitHubTestRepo/CredScanGitHubTestRepo/test.azStorageAccount.cs
@@ -8,7 +8,7 @@ namespace CredScanGitHubTestRepo
 {
     class storageAccount
     {
-        static string ConnStr = "AccountName=test;AccountKey=abcdefghijklmnopqrstuvwxyz0123456789/+ABCDEabcdefghijklmnopqrstuvwxyz0123456789/+ABCDE==;EndpointSuffix=core.windows.net;";
-        string connstr2 = "BlobEndpoint=http://test.blob.core.windows.net/;QueueEndpoint=http://test.queue.core.windows.net/;TableEndpoint=http://test.table.core.windows.net/;AccountName=test;AccountKey=abcdefghijklmnopqrstuvwxyz0123456789/+ABCDEabcdefghijklmnopqrstuvwxyz0123456789/+ABCDE==";
+            static string ConnStr = "AccountName=test;AccountKey=abcdefghijklmnopqrstuvwxyz0123456789/+ABCDEabcdefghijklmnopqrstuvwxyz0123456789/+ABCDE==;EndpointSuffix=core.windows.net;";
+            string connstr2 = "BlobEndpoint=http://test.blob.core.windows.net/;QueueEndpoint=http://test.queue.core.windows.net/;TableEndpoint=http://test.table.core.windows.net/;AccountName=test;AccountKey=abcdefghijklmnopqrstuvwxyz0123456789/+ABCDEabcdefghijklmnopqrstuvwxyz0123456789/+ABCDE==";
     }
 }
