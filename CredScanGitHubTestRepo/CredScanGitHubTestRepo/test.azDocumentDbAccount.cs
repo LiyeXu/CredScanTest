e2edd65bb93da66f3f42be1008bb954120972a26
Eric Xu <ericxu@microsoft.com>
2018-02-06 15:21:13 -0800

    update test file

--- CredScanGitHubTestRepo/CredScanGitHubTestRepo/test.azDocumentDbAccount.cs
+++ CredScanGitHubTestRepo/CredScanGitHubTestRepo/test.azDocumentDbAccount.cs
@@ -10,6 +10,7 @@ namespace CredScanGitHubTestRepo
     class TestAzureDocumentDbAccount
     {
            // This is a mockup sql azure connection string to be detected.
-           string connstr = "AccountEndpoint=https://test.documents.azure.com:443/;AccountKey=abcdefghijklmnopqrstuvwxyz0123456789/+ABCDEabcdefghijklmnopqrstuvwxyz0123456789/+ABCDE==;";
+           string connstr = "AccountEndpoint=https://test.documents.azure.com:443/;AccountKey=abcdefghijklmnopqrstuvwxyz0123456789/+ABCDEabcdefghijklmnopqrstuvwxyz0123456789/+ABCDE==;";          
+
     }
 }
--- CredScanGitHubTestRepo/CredScanGitHubTestRepo/test.azSqlConnStr.cs
+++ CredScanGitHubTestRepo/CredScanGitHubTestRepo/test.azSqlConnStr.cs
@@ -12,6 +12,14 @@ namespace CredScanGitHubTestRepo
            // This is a mockup sql azure connection string to be detected.
            string connstr = "Data Source=tcp:aaa.database.windows.net,1433;Initial Catalog=bbb;Integrated Security=False;User ID=ccc@aaa;Password=ddd;";
            string connstr2 = "server=tcp:eee.database.windows.net,1433;database=fff;Integrated Security=False;user=ggg@aaa;pwd=hhh;";
-           string connstr3 = "server=tcp:credscandev.database.windows.net,1433;database=credscandev;Integrated Security=False;user=credscansmoketestuser;pwd=credscansmoketestpwd;";
+           string connstr3 = "server=tcp:aaa.database.windows.net,1433;database=credscandev;Integrated Security=False;user=credscansmoketestuser;pwd=credscansmoketestpwd;";		   
+		   string connstr4 = @"{
+  ""database_hostname"": ""tcp:aaa.database.windows.net"",
+  ""database_username"": ""aaa"",
+  ""database_password"": ""credscansmoketestpwd"",
+  ""database_databasename"": ""DLWorkspaceJobs"",
+}";
+			string connstr5 = "jdbc:sqlserver://aaa.database.windows.net:1433;" +   
+			"databaseName=aaa_bbb;user=theextradbuser;password=credscansmoketestpwd";
     }
 }
