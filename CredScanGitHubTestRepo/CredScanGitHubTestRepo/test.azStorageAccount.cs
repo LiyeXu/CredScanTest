fecb139f0e6111ed5f710199bde738e6c2915039
Eric Xu <ericxu@microsoft.com>
2020-10-13 11:32:41 -0700

    Update test.azStorageAccount.cs

--- CredScanGitHubTestRepo/CredScanGitHubTestRepo/test.azStorageAccount.cs
+++ CredScanGitHubTestRepo/CredScanGitHubTestRepo/test.azStorageAccount.cs
@@ -9,6 +9,7 @@ namespace CredScanGitHubTestRepo
     class storageAccount
     {
             static string ConnStr = "AccountName=test;AccountKey=abcdefghijklmnopqrstuvwxyz0123456789/+ABCDEabcdefghijklmnopqrstuvwxyz0123456789/+ABCDE==;EndpointSuffix=core.windows.net;";
-string connstr2 = "BlobEndpoint=http://test.blob.core.windows.net/;QueueEndpoint=http://test.queue.core.windows.net/;TableEndpoint=http://test.table.core.windows.net/;AccountName=test;AccountKey=abcdefghijklmnopqrstuvwxyz0123456789/+ABCDEabcdefghijklmnopqrstuvwxyz0123456789/+ABCDE==";
+            string connstr2 = "BlobEndpoint=http://test.blob.core.windows.net/;QueueEndpoint=http://test.queue.core.windows.net/;TableEndpoint=http://test.table.core.windows.net/;AccountName=test;AccountKey=abcdefghijklmnopqrstuvwxyz0123456789/+ABCDEabcdefghijklmnopqrstuvwxyz0123456789/+ABCDE==";
+            string connstr3 = "DefaultEndpointsProtocol=https;AccountName=test20201013;AccountKey=tPH/EGbQZJoCcwhAmndlTrlVifhFNvLfp+wwHVXfIonmdkwbG/83MxcX6R3DLyUrb0hxA59eb8g1+GONv781Rw==;EndpointSuffix=core.windows.net";
     }
 }
